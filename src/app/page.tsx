ccde9f5f4259b18a016738d6cd7754b54ba35b50
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-27 23:45:28 +0530

    fixed api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import type { Metadata } from 'next';
-export const metadata:Metadata = {
+import { Montserrat } from "next/font/google"
+const montserrat = Montserrat({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+
+})
+export const metadata: Metadata = {
   title: 'tailwind turbopack',
   description: 'A simple, fast, and powerful web development tool',
-  
+
 };
 
-export default async function Home() {
-  let data=await fetch("/api/hello")
-  data=await data.json();
-  console.log(data);
+export default function Home() {
   return (
-    <div className={`grid bg-black w-full h-screen place-items-center`}>
-      <h1 className="font-montserrat text-3xl text-red-500">hiii</h1>
+    <div className={`${montserrat.variable} grid bg-black w-full h-screen place-items-center`}>
+      <h1 className="font-montserrat text-3xl text-red-500">Dashboard</h1>
     </div>
   );
 }
