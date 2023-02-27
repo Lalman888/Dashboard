854be066c2d27786f1f03591b977439ff82c40c1
Lalman <chauhanlakshay074@gmail.com>
2023-02-27 11:04:59 +0530

    initialized project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import type { Metadata } from 'next';
+export const metadata:Metadata = {
+  title: 'tailwind turbopack',
+  description: 'A simple, fast, and powerful web development tool',
+  
+};
+
+export default async function Home() {
+  let data=await fetch("/api/hello")
+  data=await data.json();
+  console.log(data);
   return (
     <div className={`grid bg-black w-full h-screen place-items-center`}>
-      <h1 className="font-montserrat text-3xl text-red-500">Turbopack</h1>
+      <h1 className="font-montserrat text-3xl text-red-500">hiii</h1>
     </div>
   );
 }
