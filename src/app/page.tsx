ea1ef651ffdb4e4a92bc54c3e36201e24922b3b7
Lalman <chauhanlakshay074@gmail.com>
2023-02-28 15:15:03 +0530

    initialized shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Dashboard from 'components/Dashboard';
+import Dashboard from '@/components/Dashboard';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className='divider h-screen w-full'>
       <aside className='bg-red-500 px-7'>
-        <h1 className='h-96 bg-pink-500'>hiii</h1>
+        <h1 className='h-96'>hiii</h1>
       </aside>
     <Dashboard/>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: [  "./app/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  // Or if using `src` directory:
+  "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {
       fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
         montserrat: ["var(--font-montserrat)", "sans-serif"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}
